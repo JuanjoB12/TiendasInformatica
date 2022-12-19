1080a8abeeb8f46c1343e512cbc4131b099a695e
juanj <a20juanjbe@iesluicarrillodesotomayor.org>
2022-12-19 12:38:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tiendasinformatica/basesdedatos/EditarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tiendasinformatica.basesdedatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tiendasinformatica.R
+
+class EditarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(.layout.activity_editar)
+    }
+}
---
+++ app/src/main/java/com/example/tiendasinformatica/basesdedatos/ListaDao.kt
@@ -0,0 +1,4 @@
+package com.example.tiendasinformatica.basesdedatos
+
+interface ListaDao {
+}
